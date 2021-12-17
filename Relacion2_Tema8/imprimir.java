e17c00c654be747865a804c97a6b60a8c12f3372
JimCVR <80890402+JimCVR@users.noreply.github.com>
2021-12-17 10:34:51 +0100

    Delete Relacion2_Tema8 directory

--- Relacion2_Tema8/imprimir.java
+++
@@ -1,9 +0,0 @@
-package Relacion2_Tema8;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public interface imprimir {
-    public  void imp_pantalla();
-    public void guardar_a_fichero(String ruta_fichero) throws IOException;
-}
